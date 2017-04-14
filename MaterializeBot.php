c36c887d2a9906bc3d055182828af590dcc9bf40
Noah Overcash <smileytechguy@smileytechguy.com>
2017-04-14 13:43:56 -0400

    Materialize bot: initial framework, library init

--- MaterializeBot.php
+++ MaterializeBot.php
@@ -1,3 +1,47 @@
 <?php
 
+define("DEBUG", true);
 
+if (DEBUG) {
+	error_reporting(E_ALL);
+}
+
+// load libraries
+require_once("vendor/autoload.php");
+use \Github\Client;
+
+if(!is_file("config.json")) {
+	die("You have not created a config.json file yet.\n");
+}
+
+require_once("config.php");
+
+class MaterialBot {
+    private $client;
+	private $repository;
+    private $openIssues, $closedIssues;
+
+	public function __construct($repository) {
+        $this->repository = $repository;
+
+		$this->client = new Client();
+
+		// login
+		$authentication = json_decode(file_get_contents("config.json"), true);
+		$this->client->authenticate($authentication["username"], $authentication["password"], Client::AUTH_HTTP_PASSWORD);
+
+		$this->refreshIssues();
+
+        if (DEBUG) {
+            echo "\$this->client at ".__LINE__." in ".__FILE__."\n";
+            var_dump($this->client);
+        }
+	}
+
+	protected function refreshIssues(){
+        $this->closedIssues = $this->client->api("issue")->all($this->repository[0], $this->repository[1], Array("state" => "closed"));
+        $this->openIssues = $this->client->api("issue")->all($this->repository[0], $this->repository[1], Array("state" => "open"));
+	}
+}
+
+$bot = new MaterialBot($repository);
