f1666624b9add88ff728ca7f0cc9d5573faf14d3
Noah Overcash <smileytechguy@smileytechguy.com>
2017-04-14 17:57:14 -0400

    segment and misc fixes

--- MaterializeBot.php
+++ MaterializeBot.php
@@ -34,6 +34,8 @@ class MaterialBot {
 
         $this->updateIssueCounter();
 
+        echo "Bot started...running as of issue ".$this->highestAnalyzedIssueNumber;
+
         $this->run();
     }
 
@@ -52,11 +54,9 @@ class MaterialBot {
         while (true) {
             $this->refreshIssues();
 
-            $this->highestAnalyzedIssueNumber = 1;
-
             $this->analyzeOpenIssues();
             
-            sleep(20);
+            sleep(2);
         }
     }
 
@@ -78,79 +78,103 @@ class MaterialBot {
         return self::getHTMLErrors("<!DOCTYPE html><head><title>null</title></head><body>".$in."</body></html>");
     }
 
+    public static function getCodepenStatement(array $issue, bool &$hasIssues) : string {
+        $statement = "";
+        if (preg_match_all("/http(s|)\:\/\/(www\.|)codepen\.io\/[a-zA-Z0-9\-]+\/(pen|details|full|pres)\/[a-zA-Z0-9]+/", $issue["body"], $codepens)) {
+            $links = $codepens[0];
+
+            if (count($links) == 1) {
+                $statement .= "Your codepen at ".$links[0]." is greatly appreciated!  \n";
+
+                $errors = self::getHTMLBodyErrors(file_get_contents($links[0].".html"));
+
+                if (count($errors) > 0) {
+                    $statement .= "However, it has a few issues, some of which may be not applicable:  \n";
+                    $hasIssues = true;
+                }
+                foreach ($errors as $error) {
+                    if (strlen($error) != 0) {
+                        $statement .= "* HTML ".$error."  \n";
+                    }
+                }
+
+                $statement .= "  \n";
+            } else {
+                $statement .= "Your codepens ";
+                foreach ($links as $link) {
+                    $statement .= $link." ";
+                }
+                $statement .= "are greatly appreciated!  \n";
+                $i = 1;
+
+                $codepensHaveErrors = false;
+
+                foreach ($links as $link) {
+                    $errors = self::getHTMLBodyErrors(file_get_contents($link.".html"));
+                    if (count($errors) > 0) {
+                        $codepensHaveErrors = true;
+                        $hasIssues = true;
+                    }
+                }
+
+                if ($codepensHaveErrors) {
+                    $statement .= "However, they have a few issues, some of which may be not applicable:  \n";
+                }
+
+                foreach ($links as $link) {
+                    $errors = self::getHTMLBodyErrors(file_get_contents($link.".html"));
+
+                    foreach ($errors as $error) {
+                        if (strlen($error) != 0) {
+                            $statement .= "* Codepen [".$i."](".$link.") HTML ".$error."  \n";
+                        }
+                    }
+
+                    $i++;
+                    $statement .= "  \n";
+                }
+            }
+        }
+        return $statement."  \n";
+    }
+
+    public static function getEmptyStatement(array $issue, bool &$hasIssues) : string {
+        if (strlen($issue["body"]) == 0) {
+            $statement  = "Your issue body is empty.  \n";
+            $statement .= "Please add more information in order to allow us to quickly categorize and resolve the issue.  \n  \n";
+            $hasIssues = true;
+            return $statement;
+        }
+        return "";
+    }
+
+    protected function analyzeIssue(array $issue) {
+        $hasIssues = false;
+
+        $statement  = "@".$issue["user"]["login"].",  \n";
+        $statement .= "Thank you for creating an issue!  \n\n";
+
+        $statement .= self::getEmptyStatement($issue, $hasIssues);
+        $statement .= self::getCodepenStatement($issue, $hasIssues);
+
+        if ($hasIssues) {
+            $statement .= "Please fix the above issues and re-write your example so we at Materialize can verify it’s a problem with the library and not with your code, and further proceed fixing your issue.  \n";
+        }
+
+        $statement .= "Thanks!  \n";
+        $statement .= "  \n";
+        $statement .= "(Note: This is a fully automated comment.)  \n";
+
+        $this->client->api("issue")->comments()->create($this->repository[0], $this->repository[1], $issue["number"], array("body" => $statement));
+
+        echo "Issue ".$issue["number"]." has been analyzed and commented on.\n";
+    }
+
     protected function analyzeOpenIssues() {
         $issues = $this->getUnanalyzedIssues();
 
         foreach ($issues as $issue) {
-            $hasIssues = false;
-
-            $statement  = "@".$issue["user"]["login"].",  \n";
-            $statement .= "Thank you for creating an issue!  \n\n";
-
-            // codepens
-            if (preg_match_all("/http(s|)\:\/\/(www\.|)codepen\.io\/[a-zA-Z0-9]+\/(pen|details|full|pres)\/[a-zA-Z0-9]+/", $issue["body"], $codepens)) {
-                $links = $codepens[0];
-
-                if (count($links) == 1) {
-                    $statement .= "Your codepen at ".$links[0]." is greatly appreciated!  \n";
-
-                    $errors = self::getHTMLBodyErrors(file_get_contents($links[0].".html"));
-
-                    if (count($errors) > 0) {
-                        $statement .= "However, it has a few issues, some of which may be not applicable:  \n";
-                        $hasIssues = true;
-                    }
-                    foreach ($errors as $error) {
-                        $statement .= "* ".$error."  \n";
-                    }
-
-                    $statement .= "  \n";
-                } else {
-                    $statement .= "Your codepens ";
-                    foreach ($links as $link) {
-                        $statement .= $link." ";
-                    }
-                    $statement .= "are greatly appreciated!  \n";
-                    $i = 1;
-
-                    $codepensHaveErrors = false;
-
-                    foreach ($links as $link) {
-                        $errors = self::getHTMLBodyErrors(file_get_contents($link.".html"));
-                        if (count($errors) > 0) {
-                            $codepensHaveErrors = true;
-                            $hasIssues = true;
-                        }
-                    }
-
-                    if ($codepensHaveErrors) {
-                        $statement .= "However, they have a few issues, some of which may be not applicable:  \n";
-                    }
-
-                    foreach ($links as $link) {
-                        $errors = self::getHTMLBodyErrors(file_get_contents($link.".html"));
-
-                        foreach ($errors as $error) {
-                            $statement .= "* Codepen [".$i."](".$link.") ".$error."  \n";
-                        }
-
-                        $i++;
-                        $statement .= "  \n";
-                    }
-                }
-            }
-
-            if ($hasIssues) {
-                $statement .= "Please fix the above issues and re-write your example so we at Materialize can verify it’s a problem with the library and not with your code, and further proceed fixing your issue.  \n";
-            }
-
-            $statement .= "Thanks!  \n";
-            $statement .= "  \n";
-            $statement .= "(Note: This is a fully automated comment.)  \n";
-
-            echo $statement;
-
-            $this->client->api("issue")->comments()->create($this->repository[0], $this->repository[1], $issue["number"], array("body" => $statement));
+            $this->analyzeIssue($issue);
         }
 
         $this->updateIssueCounter();
